93ac74f8b12d3f9f0ca3a7117a592dd6bd282731
Rohit Kumar Solanki <128624164+rohitsolanki401@users.noreply.github.com>
2024-12-15 20:35:40 +0530

    updates

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -81,9 +81,12 @@ const applyFilter = () => {
           {filterDoc.map((item, index) => (
             <div
               key={index}
-              onClick={() => navigate(`/Appointment/${item._id}`)}
+              onClick={() => { navigate(`/Appointment/${item._id}`);
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
               className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             >
+
               <img className="bg-blue-50" src={item.image} alt="" />
               <div className="p-4">
                 <div className="flex items-center gap-2 text-sm text-center text-green-500">
